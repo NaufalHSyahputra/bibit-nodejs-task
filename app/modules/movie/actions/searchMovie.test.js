3fa7e28116e79005b66a2b3860b35bc692d560df
Naufal Hakim Syahputra <naufalhsyahputra@gmail.com>
2021-08-25 19:58:18 +0700

    Add searchMovie Test

--- app/modules/movie/actions/searchMovie.test.js
+++ app/modules/movie/actions/searchMovie.test.js
@@ -1,8 +1,9 @@
 /* eslint-disable import/extensions */
 // eslint-disable-next-line import/extensions
-// const { getMockReq, getMockRes } = require("@jest-mock/express");
+// eslint-disable-next-line import/no-unresolved
+const { getMockReq, getMockRes } = require("@jest-mock/express");
 const getData = require("../services/index.js");
-// const searchMovie = require("./searchMovie.js");
+const searchMovie = require("./searchMovie.js");
 /* eslint-disable no-undef */
 describe("Search Movie Tests", () => {
   it("return 401 when using invalid api key", async () => {
@@ -12,5 +13,16 @@ describe("Search Movie Tests", () => {
     });
     expect(result).toBeNull();
     expect(error.response.status).toBe(401);
+  });
+  it("return 422 when search value is null", async () => {
+    const req = getMockReq({ params: { s: null } });
+    const { res } = getMockRes();
+    const result = await searchMovie(req, res);
+    const responseExpected = {
+      Response: "False",
+      Error: "Search Keyword is required",
+    };
+    expect(result.status.mock.calls[0][0]).toBe(422);
+    expect(result.json.mock.calls[0][0]).toEqual(responseExpected);
   });
 });
