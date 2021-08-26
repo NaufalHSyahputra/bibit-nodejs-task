1acc2c077c6221d92fa27785d99021865df9ab20
NaufalHSyahputra <naufalhsyahputra@gmail.com>
2021-08-26 13:16:30 +0700

    Add Swagger

--- app/index.js
+++ app/index.js
@@ -2,20 +2,54 @@
 /* eslint-disable import/extensions */
 const express = require("express");
 const cors = require("cors");
+const expressJSDocSwagger = require("express-jsdoc-swagger");
 const movieRouter = require("./modules/movie/routes/index.js");
 const db = require("./models/index.js");
 const logAPICall = require("./middleware/logAPICall.js");
 
 const app = express();
+const port = process.env.NODE_LOCAL_PORT;
 
 db.sequelize.sync().then(() => {
   console.log("Sync DB Success.");
 });
 app.use(cors());
+const options = {
+  info: {
+    version: "1.0.0",
+    title: "Bibit Node JS Task",
+    license: {
+      name: "MIT",
+    },
+  },
+  security: {
+    BasicAuth: {
+      type: "http",
+      scheme: "basic",
+    },
+  },
+  baseDir: __dirname,
+  // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+  filesPattern: "./**/*.js",
+  // URL where SwaggerUI will be rendered
+  swaggerUIPath: "/api-docs",
+  // Expose OpenAPI UI
+  exposeSwaggerUI: true,
+  // Expose Open API JSON Docs documentation in `apiDocsPath` path.
+  exposeApiDocs: false,
+  // Open API JSON Docs endpoint.
+  apiDocsPath: "/v3/api-docs",
+  // Set non-required fields as nullable by default
+  notRequiredAsNullable: false,
+  // You can customize your UI options.
+  // you can extend swagger-ui-express config. You can checkout an example of this
+  // in the `example/configuration/swaggerOptions.js`
+  swaggerUiOptions: {},
+};
+expressJSDocSwagger(app)(options);
 app.get("/", (_req, res) => res.send("Hello, World!"));
-app.use("/movie", logAPICall, movieRouter);
+app.use("/api/movie", logAPICall, movieRouter);
 
-const port = process.env.NODE_LOCAL_PORT;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- app/modules/movie/routes/index.js
+++ app/modules/movie/routes/index.js
@@ -3,11 +3,39 @@ const detailMovie = require("../actions/detailMovie");
 const searchMovie = require("../actions/searchMovie");
 
 const router = express.Router();
-
+/**
+ * GET /api/movie
+ * @summary This is the summary of the endpoint
+ * @return {object} 200 - Success response
+ * @example response - 200 - example success response
+ * { success: true, message: "Hello, Movie Module" }
+ */
 router.get("/", (_req, res) => {
   res.json({ success: true, message: "Hello, Movie Module" }).status(200);
 });
+/**
+ * GET /api/movie/search
+ * @summary This is the summary of the endpoint
+ * @param {string} s.query.required - search keyword param
+ * @param {string} page.query - page param
+ * @return {object} 200 - Success response
+ * @return {object} 401 - API Error response
+ * @return {object} 422 - Validation Error response
+ * @example response - Error response
+ * { Response: "False", Error: "Invalid API key!" }
+ */
 router.get("/search", searchMovie);
+/**
+ * GET /api/movie/detail
+ * @summary This is the summary of the endpoint
+ * @param {string} i.query.required - IMDB ID param
+ * @param {string} t.query - title param
+ * @return {object} 200 - Success response
+ * @return {object} 401 - API Error response
+ * @return {object} 422 - Validation Error response
+ * @example response - Error response
+ * { Response: "False", Error: "Invalid API key!" }
+ */
 router.get("/detail", detailMovie);
 
 module.exports = router;
